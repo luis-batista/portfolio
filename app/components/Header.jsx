f4ddcd9f91a2e6effa70651446ca0611e4ba0422
luis-batista <110860952+luis-batista@users.noreply.github.com>
2025-03-19 01:44:50 -0300

    Alterando o arquivo de download

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import React from 'react'
 import { motion } from "motion/react"
 
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/portfolio' : '';
+
 const Header = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center h-screen w-11/12 mx-auto gap-8">
@@ -45,7 +48,7 @@ const Header = () => {
         initial={{y: 30, opacity: 0}}
         whileInView={{y: 0, opacity: 1}}
         transition={{duration: 0.6, delay: 1}} 
-        href="/luiseduardo_curriculo.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black font-Outfit'>
+        href={`${basePath}/luiseduardo_curriculo.pdf`} download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black font-Outfit'>
           Currículo<Image src={assets.download_icon} alt='' className='w-4'/>
         </motion.a>
 
